65e8bdf30d1dfb0cf2b810b75171d4b6dd3694bf
mzwein <148162241+mzwein@users.noreply.github.com>
2025-01-19 16:13:31 -0800

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,147 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "62zJm9vowFC":
+        Script1();
+        break;
+      case "691zROyBcXs":
+        Script2();
+        break;
+      case "695E0p9wqyK":
+        Script3();
+        break;
+      case "5UlysVOQdY9":
+        Script4();
+        break;
+      case "5kTZi7EXasq":
+        Script5();
+        break;
+      case "5ccBZ0t0hdA":
+        Script6();
+        break;
+      case "5wanHjDjfTJ":
+        Script7();
+        break;
+      case "6l1omtVz6vf":
+        Script8();
+        break;
+      case "5wgr7e6LYs6":
+        Script9();
+        break;
+      case "65epgWZfADF":
+        Script10();
+        break;
+      case "6l3AtroZITs":
+        Script11();
+        break;
+      case "6hgU8p3ZkdR":
+        Script12();
+        break;
+      case "5h5dp9davss":
+        Script13();
+        break;
+      case "6R8ye82TgXx":
+        Script14();
+        break;
+  }
+}
+
+function Script1()
+{
+  var randomnumber = Math.floor((Math.random()*6)+1);
+var player = GetPlayer();
+player.SetVar("randnum",randomnumber);
+}
+
+function Script2()
+{
+  var randomnumber = Math.floor((Math.random()*6)+1);
+var player = GetPlayer();
+player.SetVar("randnum",randomnumber);
+}
+
+function Script3()
+{
+  var randomnumber = Math.floor((Math.random()*6)+1);
+var player = GetPlayer();
+player.SetVar("randnum",randomnumber);
+}
+
+function Script4()
+{
+  var randomnumber = Math.floor((Math.random()*6)+1);
+var player = GetPlayer();
+player.SetVar("randnum",randomnumber);
+}
+
+function Script5()
+{
+  var randomnumber = Math.floor((Math.random()*6)+1);
+var player = GetPlayer();
+player.SetVar("randnum",randomnumber);
+}
+
+function Script6()
+{
+  var randomnumber = Math.floor((Math.random()*6)+1);
+var player = GetPlayer();
+player.SetVar("randnum",randomnumber);
+}
+
+function Script7()
+{
+  var randomnumber = Math.floor((Math.random()*6)+1);
+var player = GetPlayer();
+player.SetVar("randnum",randomnumber);
+}
+
+function Script8()
+{
+  var randomnumber = Math.floor((Math.random()*6)+1);
+var player = GetPlayer();
+player.SetVar("randnum",randomnumber);
+}
+
+function Script9()
+{
+  var randomnumber = Math.floor((Math.random()*6)+1);
+var player = GetPlayer();
+player.SetVar("randnum",randomnumber);
+}
+
+function Script10()
+{
+  var randomnumber = Math.floor((Math.random()*6)+1);
+var player = GetPlayer();
+player.SetVar("randnum",randomnumber);
+}
+
+function Script11()
+{
+  var randomnumber = Math.floor((Math.random()*6)+1);
+var player = GetPlayer();
+player.SetVar("randnum",randomnumber);
+}
+
+function Script12()
+{
+  var randomnumber = Math.floor((Math.random()*6)+1);
+var player = GetPlayer();
+player.SetVar("randnum",randomnumber);
+}
+
+function Script13()
+{
+  var randomnumber = Math.floor((Math.random()*6)+1);
+var player = GetPlayer();
+player.SetVar("randnum",randomnumber);
+}
+
+function Script14()
+{
+  var randomnumber = Math.floor((Math.random()*6)+1);
+var player = GetPlayer();
+player.SetVar("randnum",randomnumber);
+}
+
